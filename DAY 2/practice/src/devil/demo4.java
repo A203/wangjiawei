54c6d3542c81b5689aa93b30c0297c419571057a
wangjiawei123 <2865823242@qq.com>
2015-07-07 19:07:25 +0800

    day 02
    第二天练习

---
+++ DAY 2/practice/src/devil/demo4.java
@@ -0,0 +1,17 @@
+package devil;
+
+public class demo4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] integers={1,2,3,4};
+		for(int i=0;i<integers.length;i++){
+			System.out.println(integers[i]);
+			System.out.println();
+		}
+	}
+
+}
